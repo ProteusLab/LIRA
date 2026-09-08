59785d514101677128f6de9ee25bf58b63b77704
uslstenn <shurygin0171@gmail.com>
2026-09-09 00:35:12 +0300

    [scripts, integration] Run integration tests in scripts

--- scripts/ruby.sh
+++ scripts/ruby.sh
@@ -7,3 +7,4 @@ ruby -I ruby -I ruby/lib ruby/tests/unit/test_arch_ser_yaml.rb
 ruby -I ruby -I ruby/lib ruby/tests/unit/test_ir_builder.rb
 ruby -r simplecov -I ruby -I ruby/lib -e 'SimpleCov.start; Dir["ruby/tests/unit/*.rb"].each { |f| require_relative f }'
 bash scripts/round_trip.sh "ruby ruby/tests/integration/copy.rb"
+ruby -I ruby -I ruby/lib ruby/tests/integration/test_integration.rb
